bf00c828de721191c47b0796b58118cab084aa7d
yamilabelen98 <yami40521@gmail.com>
2024-09-01 04:23:13 -0300

    Agregado Spotify, optimizadas las funciones y reordenado archivos

--- index.js
+++ index.js
@@ -1,16 +1,55 @@
-const { Client, Intents, REST, Routes } = require("discord.js");
+const { Client, REST, Routes } = require("discord.js");
 require("dotenv").config();
+// Si deseas anular la utilización de un cliente de Spotify comenta esta línea y la que está en la linea debajo
+require("./spotifyClient");
+const { spotifyApi } = require("./spotifyClient");
+
+const token = process.env.TOKEN;
+const clientId = process.env.CLIENT_ID;
+const guildId = process.env.GUILD_ID;
+
+const commandsBody = [
+  {
+    name: "avatar",
+    description: "Muestra tu foto de perfil",
+  },
+  {
+    name: "hola",
+    description: "Responde con Hola",
+  },
+  {
+    name: "youtube",
+    description: "Busca en YouTube",
+    options: [
+      {
+        name: "buscar",
+        type: 3, // Tipo STRING
+        description: "El término que quieres buscar en YouTube",
+        required: true,
+      },
+    ],
+  },
+  // Si deseas anular la utilización de spotify comenta este comando
+  {
+    name: "spotify",
+    description: "Busca una canción en Spotify",
+    options: [
+      {
+        name: "cancion",
+        type: 3, // Tipo STRING
+        description: "El nombre de la canción que quieres buscar",
+        required: true,
+      },
+    ],
+  },
+];
 
 const client = new Client({
   intents: [53608447],
   partials: ["USER", "MESSAGE", "GUILD_MEMBER", "THREAD_MEMBER"],
 });
 
-const token = process.env.token;
-const clientId = process.env.clientId;
-const guildId = process.env.guildId;
-
-client.once("ready", () => {
+client.once("ready", async () => {
   console.log("El bot se inició correctamente");
 
   // Registra los comandos en el servidor
@@ -21,28 +60,7 @@ client.once("ready", () => {
       console.log("Empezando a registrar los comandos");
 
       await rest.put(Routes.applicationGuildCommands(clientId, guildId), {
-        body: [
-          {
-            name: "avatar",
-            description: "Muestra tu foto de perfil",
-          },
-          {
-            name: "hola",
-            description: "Responde con Hola",
-          },
-          {
-            name: "youtube",
-            description: "Busca en YouTube",
-            options: [
-              {
-                name: "buscar",
-                type: 3, // Tipo STRING
-                description: "El término que quieres buscar en YouTube",
-                required: true,
-              },
-            ],
-          },
-        ],
+        body: commandsBody,
       });
 
       console.log("Comandos registrados correctamente");
@@ -55,50 +73,94 @@ client.once("ready", () => {
 client.on("interactionCreate", async (interaction) => {
   if (!interaction.isCommand()) return;
 
-  try {
-    const { commandName, options } = interaction;
+  const { commandName, options } = interaction;
 
-    if (commandName === "hola") {
-      console.log("Recibido el comando /hola");
-      await interaction.reply("Hola");
-    } else if (commandName === "avatar") {
-      console.log("Recibido el comando /avatar");
-      const user = interaction.user;
-      await interaction.reply(
-        //interaction.reply=> responde a la interacción
-        user.displayAvatarURL({ format: "png", size: 512 })
+  // Mapa de comandos a funciones
+  const commandHandlers = {
+    hola: handleHolaCommand,
+    avatar: handleAvatarCommand,
+    youtube: handleYoutubeCommand,
+    spotify: handleSpotifyCommand,
+  };
+
+  // Verifica si existe un handler para el comando recibido
+  const commandHandler = commandHandlers[commandName];
+  if (commandHandler) {
+    try {
+      await commandHandler(interaction, options);
+    } catch (error) {
+      console.error(
+        `Error al manejar el comando /${commandName}:`,
+        error.message,
+        error.stack
       );
-    } else if (commandName === "youtube") {
-      console.log("Recibido el comando /youtube");
-
-      // Verificar si la opción que busco es válida
-      const searchTerm = options.getString("buscar");
-      if (!searchTerm) {
-        throw new Error("No se pudo obtener el término de búsqueda.");
+      if (!interaction.replied && !interaction.deferred) {
+        await interaction.reply({
+          content: "Hubo un error al procesar tu solicitud.",
+          ephemeral: true,
+        });
       }
+    }
+  }
+});
 
-      const youtubeSearchUrl = `https://www.youtube.com/results?search_query=${encodeURIComponent(
-        searchTerm
-      )}`;
-      console.log(`Generando enlace de búsqueda: ${youtubeSearchUrl}`);
+// Funciones manejadoras para cada comando
+async function handleHolaCommand(interaction) {
+  console.log("Recibido el comando /hola");
+  await interaction.reply("Hola");
+}
 
+async function handleAvatarCommand(interaction) {
+  console.log("Recibido el comando /avatar");
+  const user = interaction.user;
+  await interaction.reply(user.displayAvatarURL({ format: "png", size: 512 }));
+}
+
+async function handleYoutubeCommand(interaction, options) {
+  console.log("Recibido el comando /youtube");
+  const searchTerm = options.getString("buscar");
+  const youtubeSearchUrl = `https://www.youtube.com/results?search_query=${encodeURIComponent(
+    searchTerm
+  )}`;
+  console.log(`Generando enlace de búsqueda: ${youtubeSearchUrl}`);
+  await interaction.reply(
+    `Aquí está tu búsqueda en YouTube: ${youtubeSearchUrl}`
+  );
+}
+
+async function handleSpotifyCommand(interaction, options) {
+  console.log("Recibido el comando /spotify");
+
+  try {
+    const searchTerm = options.getString("cancion");
+    const result = await spotifyApi.searchTracks(searchTerm);
+
+    if (result.body.tracks.items.length > 0) {
+      const track = result.body.tracks.items[0];
+      const trackUrl = track.external_urls.spotify;
       await interaction.reply(
-        `Aquí está tu búsqueda en YouTube: ${youtubeSearchUrl}`
+        `Aquí está la canción que encontré: [${
+          track.name
+        }](${trackUrl}) por ${track.artists
+          .map((artist) => artist.name)
+          .join(", ")}`
       );
+    } else {
+      await interaction.reply("No encontré ninguna canción con ese nombre.");
     }
   } catch (error) {
     console.error(
-      "Error al manejar la interacción:",
+      "Error al manejar la búsqueda en Spotify:",
       error.message,
       error.stack
     );
     if (!interaction.replied && !interaction.deferred) {
       await interaction.reply({
-        content: "Hubo un error al procesar tu solicitud.",
+        content: `Hubo un error al procesar tu solicitud: ${error.message}`,
         ephemeral: true,
       });
     }
   }
-});
+}
 
 client.login(token);
---
+++ spotifyClient.js
@@ -0,0 +1,33 @@
+const SpotifyWebApi = require("spotify-web-api-node");
+require("dotenv").config();
+const spotifyClientId = process.env.SPOTIFY_CLIENT_ID;
+const spotifyClientSecret = process.env.SPOTIFY_CLIENT_SECRET;
+const spotifyApi = new SpotifyWebApi({
+  clientId: spotifyClientId,
+  clientSecret: spotifyClientSecret,
+  redirectUri: "https://open.spotify.com/playlist/0GZLOcH2gdCQEbYSdfmfMm",
+});
+
+async function getSpotifyAccessToken() {
+  try {
+    console.log("Spotify API arranca");
+    const data = await spotifyApi.clientCredentialsGrant();
+    console.log(
+      "Nuevo token de acceso de Spotify obtenido, expira en",
+      data.body["expires_in"],
+      "segundos."
+    );
+    spotifyApi.setAccessToken(data.body["access_token"]);
+  } catch (error) {
+    console.error(
+      "Error al obtener el token de Spotify:",
+      error.message,
+      error.stack
+    );
+  }
+}
+
+// Llama a getSpotifyAccessToken para inicializar el token de acceso}
+getSpotifyAccessToken();
+setInterval(getSpotifyAccessToken, 3600000);
+module.exports = { spotifyApi };
